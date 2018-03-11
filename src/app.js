2d78282a8c0e0ac25d2f0d754de944cf7caecfd0
Clint Milner <clinton.milner@gmail.com>
2018-03-11 21:33:30 +0000

    getting started with redux 101 - redux store dispatch calls and handlers within store

--- src/app.js
+++ src/app.js
@@ -4,6 +4,6 @@ import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-console.log('Section 9 Lecture 81 - app.js has loaded and is running and loading via webpack-dev-server');
+console.log('Section 10 Lecture 87 - app.js has loaded and is running and loading via webpack-dev-server');
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
