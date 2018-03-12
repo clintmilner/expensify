5fddf457334a421ac4452bdd44fab4abbb2df78b
Clint Milner <clinton.milner@gmail.com>
2018-03-12 21:24:22 +0000

    action generators and object/array destructuring

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,13 +2,41 @@ console.log( 'redux 101' );
 
 import { createStore } from 'redux';
 
+
+
+// Action Generators
+// create Action Objects
+
+const incrementCount = ({incrementBy = 1} = {}) => ({
+    type: 'INCREMENT',
+    incrementBy : incrementBy // this could be destructured more, but kept like this for clarity
+});
+
+const decrementCount = ({decrementBy = 1} = {}) => ({
+    type: 'DECREMENT',
+    decrementBy: decrementBy
+});
+
+const resetCount = () => ({
+    type: 'RESET'
+});
+
+const setCount = ({count}) => ({
+    type: 'SET',
+    count
+});
+
+
 const store = createStore( (state={ count:0 }, action) => {
 
     switch (action.type){
         case 'INCREMENT':
-            return { count: state.count + 1 };
+            return { count: state.count + action.incrementBy };
         case 'DECREMENT':
-            return { count: state.count - 1 };
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
+            return { count: state.count - decrementBy };
+        case 'SET':
+            return { count: action.count };
         case 'RESET':
             return { count: 0 };
         default:
@@ -16,22 +44,17 @@ const store = createStore( (state={ count:0 }, action) => {
     }
 });
 
-console.info(store.getState());
 
-store.dispatch(
-    { type: 'INCREMENT' }
-);
+store.subscribe( () => {
+    console.log( 'store changed', store.getState() );
+});
 
-console.info(store.getState());
+store.dispatch( incrementCount() ); // 1
+store.dispatch( incrementCount({ incrementBy: 5 })); // 6
 
-store.dispatch(
-    { type: 'RESET' }
-);
+store.dispatch( resetCount() ); // 0
 
-console.info(store.getState());
+store.dispatch( decrementCount()); // -1
+store.dispatch( decrementCount({ decrementBy: 10 })); // -11
 
-store.dispatch(
-    { type: 'DECREMENT' }
-);
-
-console.warn(store.getState());
+store.dispatch( setCount({ count: 311 })); // 311
