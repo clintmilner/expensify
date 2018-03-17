c7079f4a3212430fd0c3fa1fdc112de78fb68ee2
Clint Milner <clinton.milner@gmail.com>
2018-03-17 20:03:38 +0000

    add expense

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
+import {connect} from "react-redux";
+import {addExpense} from "../actions/expenses";
 
-const AddExpensePage = () => (
+
+const AddExpensePage = (props) => (
     <div>
-        This is the Add Expense Page
+        <h1>Add Expense</h1>
+        <ExpenseForm
+            onExpenseSubmit={ (expense) => {
+                props.dispatch( addExpense(expense) );
+                props.history.push('/');
+            }}
+        />
     </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
