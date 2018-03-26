f85032ead59465c0f9e9fb4032914346c3e773ac
Clint Milner <clinton.milner@gmail.com>
2018-03-26 21:58:27 +0100

    cleaned up dev dependencies in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -44,7 +44,8 @@ module.exports = (env) => {
         },
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true // needed for client-side routing (react-router-dom)
+            historyApiFallback: true, // needed for client-side routing (react-router-dom)
+            publicPath: '/dist/'
         },
         plugins: [
             CSSExtract
