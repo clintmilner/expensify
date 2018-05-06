e246b54320f72b8348f5b15645b89a9d27bedac4
Clint Milner <clinton.milner@gmail.com>
2018-05-06 20:39:28 +0100

    more boring firebase stuff

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 // Firebase config
 const config = {
-    apiKey: "AIzaSyA-8YqZoOvjzWbP00-HZAIRagXRu9c-Mk8",
-    authDomain: "expensify-311.firebaseapp.com",
-    databaseURL: "https://expensify-311.firebaseio.com",
-    projectId: "expensify-311",
-    storageBucket: "expensify-311.appspot.com",
-    messagingSenderId: "718581862917"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 // Initialize Firebase
